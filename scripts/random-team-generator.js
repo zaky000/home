02c1b5707a37034a81b7b5f66c4f805c58868b42
zaky000 <147989875+zaky000@users.noreply.github.com>
2023-10-18 21:55:51 +0700

    Add files via upload

---
+++ scripts/random-team-generator.js
@@ -0,0 +1,9 @@
+const input = document.getElementById("name-input");
+const team = document.getElementById("number");
+const shuffle = document.getElementById("shuffle-button");
+const teamList = document.getElementById("output");
+const reset = document.getElementById("reset-button");
+
+reset.addEventListener("click", () => {
+    console.log("Hello")
+});
